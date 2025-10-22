93849f7bf3cad552e2a75fb9c7f432104d737803
sk-sazid <1.sksazid@gmail.com>
2025-10-22 22:32:46 +0600

    solve error

---
+++ src/main/java/com/shadhin/build_API/BuildApiApplication.java
@@ -0,0 +1,13 @@
+package com.shadhin.build_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadhin/build_API/BuildApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadhin.build_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
